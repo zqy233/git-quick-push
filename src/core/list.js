1cd0f26b3d395534ef1447b53afa52c8eefc980f
zqylzcwcxy <347891134@qq.com>
2022-07-18 20:18:07 +0800

    :sparkles:new feature : 新增git remote相关命令和git merge --no-ff命令

--- src/core/list.js
+++ src/core/list.js
@@ -4,27 +4,25 @@ const lolcat = require("@darkobits/lolcatjs")
 const inquirer = require("inquirer")
 const { emojiArr, commitArr } = require("../choices")
 
-const list = async () => {
-  const { git } = await inquirer.prompt([
+const run = command => {
+  const loading = ora("").start()
+  shell.exec(command)
+  loading.succeed("command finished")
+  list()
+}
+const gitCheckout = async () => {
+  const { input } = await inquirer.prompt([
     {
-      type: "list",
-      name: "git",
-      message: lolcat.fromString("please choose git commands"),
-      choices: ["git commit", "git pull", "git push", "exit"]
+      type: "input",
+      name: "input",
+      message: lolcat.fromString("input branch name :")
     }
   ])
-  if (git == "exit") return shell.exit(1)
-  if (!shell.which("git")) {
-    shell.echo("Sorry, you need download git first")
-    return shell.exit(1)
-  }
+  run(`git checkout -b ${input}`)
+}
+
+const gitCommit = async () => {
   shell.exec("git add .")
-  if (git == "git pull" || git == "git push") {
-    const loading = ora("").start()
-    shell.exec(git)
-    loading.succeed()
-    return list()
-  }
   const { type } = await inquirer.prompt([
     {
       type: "list",
@@ -40,7 +38,69 @@ const list = async () => {
       message: lolcat.fromString("full details :")
     }
   ])
-  shell.exec(`git commit -m "${emojiArr[commitArr.indexOf(type)]}${input ? type + ": " + input : type}"`)
-  list()
+  run(`git commit -m "${emojiArr[commitArr.indexOf(type)]}${input ? type + ": " + input : type}"`)
 }
+
+const gitRemote = async () => {
+  const { type } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "type",
+      message: lolcat.fromString("select remote command"),
+      choices: ["git remote -v", "git remote add origin", "git remote set-url origin"]
+    }
+  ])
+  if (type === "git remote -v") {
+    return run(type)
+  }
+  const { input } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "input",
+      message: lolcat.fromString("repository url:")
+    }
+  ])
+  run(`${type} ${input}`)
+}
+
+const list = async () => {
+  const { git } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "git",
+      message: lolcat.fromString("please choose git commands"),
+      choices: ["git init", "git branch", "git checkout -b", "git remote", "git commit", "git pull", "git push", "git merge --no-ff"]
+    }
+  ])
+  if (!shell.which("git")) {
+    shell.echo("Sorry, you need download git first")
+    return shell.exit(1)
+  }
+  switch (git) {
+    case "git init":
+      run(git)
+      break
+    case "git branch":
+      run(git)
+      break
+    case "git checkout -b":
+      gitCheckout()
+      break
+    case "git pull":
+      run(git)
+      break
+    case "git push":
+      run(git)
+      break
+    case "git commit":
+      gitCommit()
+      break
+    case "git remote":
+      gitRemote()
+      break
+    case "git merge --no-ff":
+      run(git)
+  }
+}
+
 module.exports = list
